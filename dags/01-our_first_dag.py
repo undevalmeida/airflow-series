97b41aaf7e2af252f20264ecee7310dbf4d16c5a
André <andrejunior573@gmail.com>
2023-12-18 23:48:58 -0300

    Construindo a primeira dag

---
+++ dags/01-our_first_dag.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+# Não tão legal
+"""
+dag = DAG(dag_id='01-our_first_dag', start_date=datetime(2023,12,18))
+
+task1 = EmptyOperator(task_id='extract',dag=dag)
+task2 = EmptyOperator(task_id='transform',dag=dag)
+task3 = EmptyOperator(task_id='load',dag=dag)
+
+task1 >> task2 >> task3
+
+"""
+
+# Boa
+
+with DAG(dag_id='01-our_first_dag', start_date=datetime(2023,12,18)) as dag:
+
+    task1 = EmptyOperator(task_id='extract',dag=dag)
+    task2 = EmptyOperator(task_id='transform',dag=dag)
+    task3 = EmptyOperator(task_id='load',dag=dag)
+
+    task1 >> task2 >> task3
+
+
+
+# ----- || --------
+    
+# Muito legal
+"""
+from airflow.decorators.python import dag, task
+
+@dag(
+        start_date
+)
+def our_first_dag():
+"""
