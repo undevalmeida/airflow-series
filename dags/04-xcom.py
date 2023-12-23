8c75b1ca0aa8839d5b1c7fa19d3dbd6c2eaf3556
André <andrejunior573@gmail.com>
2023-12-23 19:30:33 -0300

    Tratamento dos dados

--- dags/04-xcom.py
+++ dags/04-xcom.py
@@ -9,6 +9,30 @@ def ingest():
 
     df = pd.read_csv(url, delimiter=';', encoding='utf-8')
 
+    df.columns = [column.strip() for column in df.columns]
+
+    id_vars = ['COMBUSTÍVEL', 'ANO', 'REGIÃO', 'ESTADO', 'UNIDADE', 'TOTAL']
+    
+    value_vars = [column for column in df.columns if column not in id_vars]
+
+    column_to_be_cleaned = value_vars + ['TOTAL']
+
+    df_cleaned = df.copy()
+    df_cleaned = df.fillna(0)
+
+    for column in column_to_be_cleaned:
+        df_cleaned[column] = df_cleaned[column].apply(lambda x: str(x).replace('.', '')).astype(int)
+
+    df_melted = pd.melt(df_cleaned, id_vars=id_vars, value_vars=value_vars)
+    df_melted['contribution'] = df_melted['value']/df_melted['TOTAL']
+    df_melted['contribution_%'] = df_melted['value']/df_melted['TOTAL'] * 100
+
+    max_contribution = df_melted['contribution'].max()
+    min_contribution = df_melted[df_melted['contribution']!= 0]['contribution'].min()
+
+    # max_contribution_row = df_melted[df_melted['contribution']==max_contribution]
+    # min_contribution_row = df_melted[df_melted['contribution']==min_contribution]
+
 with DAG(
     dag_id='04-xcoms'
     , start_date=datetime(2023, 12, 21)) as dag:
